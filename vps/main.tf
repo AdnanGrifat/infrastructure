ad083924d23a86e13640901b971462ab58ba6af8
Adnan Grifat <147456730+AdnanGrifat@users.noreply.github.com>
2024-01-27 11:30:44 -0600

    add vpc

--- vps/main.tf
+++ vps/main.tf
@@ -1,25 +1,18 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  enable_nat_gateway = fales
+  enable_vpn_gateway = fales
 
   tags = {
-    Name = "HelloWorld"
-    managed_by = "terraform"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
